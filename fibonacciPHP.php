cf98df623414c52bd7e9724d23406e2b249edeab
Vívian Machado <113874340+VivianMachado0@users.noreply.github.com>
2023-01-19 15:54:20 -0300

    Create fibonacciPHP.php

---
+++ fibonacciPHP.php
@@ -0,0 +1,19 @@
+<?php 
+
+function Fibonacci($num){ 
+ 
+    if ($num == 0) 
+        return 0;     
+    else if ($num == 1) 
+        return 1;     
+  
+    else
+        return (Fibonacci($num-1) + Fibonacci($num-2)); 
+} 
+  
+$num = 10; 
+for ($i = 0; $i < $num; $i++){   
+    echo Fibonacci($i),' '; 
+} 
+  
+?>
